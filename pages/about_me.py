999974819bce2c68e0fca9a2dbcfbb257ea6b307
Eduardo Peña <eduardoandreeps@gmail.com>
2024-08-16 22:56:20 -0500

    Modify about me page

--- pages/about_me.py
+++ pages/about_me.py
@@ -13,13 +13,13 @@ def show_form():
 col1, col2 = st.columns(2, gap='medium', vertical_alignment='center')
 
 with col1:
-    st.image('./assets/picture.png', width=225)
+    st.image('./assets/picture.png', width=260)
 
 with col2:
     st.title('Eduardo Peña', anchor=False)
     
     st.markdown(
-        ':blue-background[ML Engineer | Data Analyst]'
+        ':violet-background[ML Engineer | Data Analyst]'
     )
     st.write(
         'Ofrezco soluciones impulsadas por IA y genero desarrollos enfocados en mejoras basadas en datos\n'
@@ -32,11 +32,12 @@ with col2:
 ## --- EXPERIENCE 
 st.write('\n')
 st.write('\n')
-st.subheader('Experiencia y Cualificaciones', anchor=False)
+st.subheader(':blue-background[Experiencia y Cualificaciones]', anchor=False)
 st.write(
     '''
     - 4 años en análisis de datos y riesgos
     - Conocimientos avanzados en Python
+    - 1 año en desarrollo de proyectos impulsados por Machine Learning y Deep Learning (Computer Vision, NLP y Clasificación de Clientes)
     - Proficiencia en estadística y diversas ramas científicas
     - Capacidades de liderazgo y trabajo en equipo, con alto sentido de iniciativa
     '''
@@ -45,13 +46,14 @@ st.write(
 
 ## --- SKILLS
 st.write('\n')
-st.subheader('Habilidades', anchor=False)
+st.subheader(':blue-background[Habilidades]', anchor=False)
 st.write(
     '''
     - Programación: Python (Tensorflow, Scikit-learn, TFX, Pandas), SQL
     - Visualización de Datos: Tableau, PowerBI, MS Excel, Matplotlib
     - Modelado ML: Random Forest, DNN, Clustering (KNN), Regressions 
     - Bases de datos: MySQL
+    - Desarrollo web: HTML, CSS, JavaScript, ReactJS, Bootstrap
     - Programación en la nube: Azure Web Services
     '''
 )
